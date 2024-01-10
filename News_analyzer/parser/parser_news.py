7fa1bd2561dca0c291dadee7c175b06c682e790e
inspiredOFF <vindsf@gmail.com>
2024-01-11 01:04:08 +0300

    add def client_reqest and responce_parsing_headers. modified main_url

--- News_analyzer/parser/parser_news.py
+++ News_analyzer/parser/parser_news.py
@@ -1,7 +1,19 @@
 import asyncio
 import aiohttp
-from bs4 import beatifulsoup
+from bs4 import BeautifulSoup
 
-main_url = "https://ria.ru/services/lenta/more.html"
+main_url = "https://ria.ru/services/archive/widget/more.html"
 
-asyadd impo
+async def client_reqest (url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as responce:
+            html = await responce.text ()
+            return html
+
+async def responce_parsing_headers (html_text):
+    soup = BeautifulSoup(html_text,'lxml')
+    url_headers = soup.find('a', class_="href")
+    print(url_headers)
+
+html_text = asyncio.run(client_reqest(main_url))
+asyncio.run(responce_parsing_headers(html_text))
