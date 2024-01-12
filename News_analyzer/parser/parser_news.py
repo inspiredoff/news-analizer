b0ca135df3ff0d5924b175d0e449b1c1657430a7
inspiredoff <vindsf@gmail.com>
2024-01-12 23:18:07 +0300

    add loop for parsing all news from the page

--- News_analyzer/parser/parser_news.py
+++ News_analyzer/parser/parser_news.py
@@ -3,17 +3,19 @@ import aiohttp
 from bs4 import BeautifulSoup
 
 main_url = "https://ria.ru/services/archive/widget/more.html"
-
+tag = 'a'
 async def client_reqest (url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as responce:
             html = await responce.text ()
             return html
 
-async def responce_parsing_headers (html_text):
+async def responce_parsing_headers (html_text, tag):
     soup = BeautifulSoup(html_text,'lxml')
-    url_headers = soup.find('a', class_="href")
-    print(url_headers)
+    url_headers = soup.find_all(tag)
+    return url_headers
 
 html_text = asyncio.run(client_reqest(main_url))
-asyncio.run(responce_parsing_headers(html_text))
+for a in asyncio.run(responce_parsing_headers(html_text, tag)):
+    print(a.get('href'))
+
