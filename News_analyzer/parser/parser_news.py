d81551c93ba81b445e8a457022ca9891b108fa72
inspiredOFF <vindsf@gmail.com>
2024-02-02 14:05:14 +0300

    adedd save parsing data in .csv file.

--- News_analyzer/parser/parser_news.py
+++ News_analyzer/parser/parser_news.py
@@ -1,9 +1,9 @@
 import asyncio
 import aiohttp
 import time
-
-import self
 from bs4 import BeautifulSoup
+import csv
+import json
 
 
 class RiaNews:
@@ -29,7 +29,7 @@ class RiaNews:
         self.class_time_headline_update = 'article__info-date-modified'
         self.itemprop_time_headline_created = 'dateCreated'
         self.itemprop_time_headline_Modified = 'dateModified'
-        self.times = "2024-02-01 21:15"
+        self.times = "2024-01-01 00:00"
         self.search_depth = time.strptime(self.times, "%Y-%m-%d %H:%M")
 
         self.tag_tag_headline = None
@@ -49,15 +49,15 @@ class NewsParser(RiaNews):
         self.url = url
         self.search_result = None
         self.article_url = []
-        self.next_page_url = None
-        self.news_headline = None
-        self.time_headline = None
-        self.tags_headline = []
+        self.headline_news = None
+        self.time_news = None
+        self.tags_news = []
         self.num_views_headline = None
 
     async def download_translate_html_to_text(self):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.url) as response:
+                # await asyncio.sleep(1)
                 html_text = await response.text()
             self.soup = BeautifulSoup(html_text, 'lxml')
 
@@ -100,11 +100,11 @@ class NewsParser(RiaNews):
     # async def filter_search_result(self):
 
     async def time_compassion(self):
-        var = self.search_depth
-        var1 = self.time_headline
-        if var >= var1:
+        if self.search_depth >= self.time_news:
             continuation = False
             return continuation
+
+    # async def save_output_data(self):
 
 
 class NewsPageParser(NewsParser):
@@ -128,8 +128,7 @@ class NewsHeadlineParsing(NewsParser):
 
     async def search_for_headline_news(self):
         await self.find_data_in_itemprop(self.tag_news_headline, self.class_news_headline)
-        self.news_headline = self.search_result[3].get(self.get_news_headline)
-        print(self.news_headline)
+        self.headline_news = self.search_result[3].get(self.get_news_headline)
 
     async def search_for_time_news(self):
         await self.find_data_in_itemprop(self.tag_time_headline_update, self.itemprop_time_headline_Modified)
@@ -139,14 +138,14 @@ class NewsHeadlineParsing(NewsParser):
             await self.find_data_in_itemprop(self.tag_time_headline, self.itemprop_time_headline_created)
             for text in self.search_result:
                 time_news = text.get_text(strip=True)
-        self.time_headline = await self.converting_string_to_time(time_news)
+        self.time_news = await self.converting_string_to_time(time_news)
 
     async def search_for_tag_news(self):
         await self.find_data_in_class(self.tag_tag_headline, self.class_tag_headline)
         for text in self.search_result:
-            self.tags_headline.append(text.get_text(strip=True))
+            self.tags_news.append(text.get_text(strip=True))
 
-    async def initialize_parser(self,list):
+    async def initialize_parser(self, list):
         await self.download_translate_html_to_text()
         await self.search_for_headline_news()
         await self.search_for_time_news()
@@ -166,72 +165,69 @@ class NumberOfHeaderViews(NewsParser):
         await self.search_for_number_views()
 
 
-# news_header_url = []
+class ResultData:
+
+    def __init__(self):
+        self.news_data = []
+
+    async def save_data_in_list(self, headline_news, time_news, tag_news, num_views_news):
+        self.news_data.append(
+            {
+                "headline_news": headline_news,
+                "time_news": time_news,
+                "tag_news": tag_news,
+                "num_views_news": num_views_news,
+            }
+        )
+
+    async def create_new_file(self):
+        with open("news.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (
+                    "заголовок статьи",
+                    "дата публикации",
+                    "тэги",
+                    "количество просмотров"
+                )
+            )
+
+    async def save_data_in_file(self):
+        for news in self.news_data:
+            with open("news.csv", "a") as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        news["headline_news"],
+                        news["time_news"],
+                        news["tag_news"],
+                        news["num_views_news"],
+                    )
+                )
 
 
 async def parser():
     url = RiaNews().main_url
     parsers = NewsParser(url)
+    result_file = ResultData()
+    await result_file.create_new_file()
     continuation = parsers.continuation
+    # await save_output_data1()
     while continuation:
         parser_global_page = NewsPageParser(url)
         await parser_global_page.initialize_parser()
         url = parser_global_page.next_page_url
         for urls in parser_global_page.article_url:
             parser_news_page = NewsHeadlineParsing(urls)
-            parser_page_views = NumberOfHeaderViews('https://ria.ru/services/dynamics' + urls[14:-1])
+            parser_page_views = NumberOfHeaderViews(f'https://ria.ru/services/dynamics{urls[14:-1]}')
             await parser_news_page.initialize_parser(parser_global_page.article_url)
             await parser_page_views.initialize_parser()
             if await parser_news_page.time_compassion() == False:
                 continuation = False
                 break
+            await result_file.save_data_in_list(parser_news_page.headline_news, parser_news_page.time_news,
+                                                parser_news_page.tags_news, parser_page_views.num_views_headline)
+        await result_file.save_data_in_file()
 
 
 asyncio.run(parser())
-
-#### асинхронный цикл ####
-
-# for page in range(1, 5):
-#     news_list = RiaNews(url)
-#     asyncio.run(news_list.parser())
-#     news_header_url = news_header_url +news_list.url_headers
-#     url = news_list.next_url
-#
-# async def async_for(news_header_url):
-#     tasks = []
-#     for url in news_header_url:
-#         task = asyncio.create_task(NewsHeadlineParsing(url).parser_1())
-#         tasks.append(task)
-#     await asyncio.gather(*tasks)
-#
-# asyncio.run(async_for(news_header_url))
-
-#### синхронный цикл ####
-#
-# for page in range(1, 5):
-#     c = RiaNews(url_)
-#     asyncio.run(c.parser())
-#     news_header_url = c.article_url
-#     print(c.article_url)
-#     url_ = c.next_page_url
-#
-#     for url in c.article_url:
-#         asyncio.run(RiaNews(url).parser_1())
-
-### полуасинхронный цикл ####
-
-# for page in range(1, 5):
-#     c = RiaNews(url_)
-#     asyncio.run(c.parser())
-#     news_header_url = c.article_url
-#     print(c.article_url)
-#     url_ = c.next_page_url
-#
-#
-#     async def async_for():
-#         tasks = []
-#         for url in news_header_url:
-#             task = asyncio.create_task(RiaNews(url).parser_1())
-#             tasks.append(task)
-#
-#         await asyncio.gather(*tasks)
